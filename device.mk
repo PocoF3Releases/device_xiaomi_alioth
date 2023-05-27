92c2f59f35dff52e4c2bc7162bdac2ac75480d14
johnmart19 <johnivan19999@gmail.com>
2023-05-27 04:45:38 +0300

    Alioth: Inherit Miui Camera Repository
    - Includes MiSys and cleaned deps
    - https://gitlab.com/johnmart19/vendor_xiaomi_camera

--- device.mk
+++ device.mk
@@ -32,6 +32,9 @@ PRODUCT_PACKAGES += \
 # Init
 $(call soong_config_set,xiaomi_kona,variant_lib,//$(LOCAL_PATH):libvariant_xiaomi_alioth)
 
+# Miui Camera
+include vendor/xiaomi/camera/miuicamera.mk
+
 # Overlays
 PRODUCT_PACKAGES += \
     ApertureOverlayDevice \
