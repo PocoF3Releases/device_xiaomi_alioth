6850195a11ceaf191dcc5e1255a2722952f86480
johnmart19 <johnivan19999@gmail.com>
2024-02-26 02:07:35 +0200

    Alioth: Minimal Evolution X bringup
    * Going to make builds for own usage

--- evolution_alioth.mk
+++ evolution_alioth.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2021 The LineageOS Project
-# Copyright (C) 2022-2023 VoidUI Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,27 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common VoidUI stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+BUILD_WITH_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_MIUI_CAMERA := true
+TARGET_INCLUDES_MIUI_CAMERA := true
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := aosp_alioth
+PRODUCT_NAME := evolution_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Rom Specific Flags
-TARGET_INCLUDE_WIFI_EXT := true
-TARGET_BOOT_ANIMATION_RES := 1440
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_SUPPORTS_QUICK_TAP  := true
-
-# VoidUI Maintainer Flags
-VOID_MAINTAINER := johnmart19
-CUSTOM_BUILD_TYPE := OFFICIAL
