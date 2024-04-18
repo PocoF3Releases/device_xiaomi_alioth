3892c016a72a547572bd95f5c71f63c317fb4a2f
johnmart19 <johnivan19999@gmail.com>
2024-04-18 07:59:37 +0300

    Alioth: Kernel: Switch to alioth_defconfig for kernel compilation

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/alioth.config
+TARGET_KERNEL_CONFIG := alioth_defconfig
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := alioth,aliothin
