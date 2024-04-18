9672cb5b6ed9ddfad4f8f110eb843b02cf7982fe
johnmart19 <johnivan19999@gmail.com>
2024-04-18 08:06:36 +0300

    Alioth: Minimal EvolutionX Bringup

--- evolution_alioth.mk
+++ evolution_alioth.mk
@@ -8,13 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+BUILD_WITH_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_SUPPORTS_QUICK_TAP := true
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+# Build Type Flags
+EVO_BUILD_TYPE = DEVELOPMENT
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := evolution_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
