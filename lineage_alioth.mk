a2731d6d9a98785d7c0313581175bece7cc6d9d5
johnmart19 <johnivan19999@gmail.com>
2024-05-12 00:47:02 +0300

    Alioth: Bringup for EvolutionXYZ based on LOS

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -8,14 +8,12 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution X stuff.
+# Inherit some common Evolution XYZ stuff.
 BUILD_WITH_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_BUILD_APERTURE_CAMERA := false
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_MIUI_CAMERA := true
-TARGET_INCLUDES_MIUI_CAMERA := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Build Type Flags
 EVO_BUILD_TYPE = DEVELOPMENT
@@ -23,7 +21,7 @@ EVO_BUILD_TYPE = DEVELOPMENT
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := evolution_alioth
+PRODUCT_NAME := lineage_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
