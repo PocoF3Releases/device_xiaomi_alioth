d93c6aedec59a0911012fcbe37856121536f603a
Joel Lucas <joelcba57@gmail.com>
2025-09-21 12:17:49 +0300

    Alioth: Spoof BuildFingerprint as Pixel Beta
    * It is necessary to pass integrity with the built-in Keybox Importer, as our stock fingerprint is not "Valid"

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -27,4 +27,4 @@ PRODUCT_MODEL := POCO F3
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=POCO/alioth_global/alioth:13/TKQ1.221114.001/V816.0.3.0.TKHMIXM:user/release-keys
+    BuildFingerprint=google/husky_beta/husky:16/BP41.250822.010/14082742:user/release-keys
